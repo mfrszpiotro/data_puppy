b28d04b97e4ac7eff196dde9c7c31a948ac52111
Marcel <marcel.piotrowski5543@gmail.com>
2025-04-05 23:06:07 +0200

    feat: unzip/cleanup for datasets with temp folder

--- app/pup.py
+++ app/pup.py
@@ -1,16 +1,25 @@
 import csv
+import shutil
+from pprint import pprint
+from pathlib import Path
+from typing import Any, Generator
+from zipfile import ZipFile
+import logging
+from contextlib import contextmanager
+
+
+@contextmanager
+def temp_unzip_dir(filepath: str) -> Generator[Path, None, None]:
+    try:
+        TEMP_DIR = Path.cwd() / "temp_zip"
+        TEMP_DIR.mkdir()
+        with ZipFile(filepath, "r") as zip_reference:
+            zip_reference.extractall(TEMP_DIR)
+        yield TEMP_DIR
+    finally:
+        shutil.rmtree(TEMP_DIR)
 
 
 class DataPuppy:
-    def __init__(self, filepath: str, mode: str = "r", timeout: int = 5):
+    def __init__(self, filepath: str):
         self.filepath = filepath
-        self.mode = mode
-        self.timeout = timeout
-
-    def __enter__(self):
-        self.opened_file = open(self.filepath, self.mode)
-        return self.opened_file
-
-    def __exit__(self, exc_type, traceback):
-        if self.opened_file:
-            self.opened_file.close()
